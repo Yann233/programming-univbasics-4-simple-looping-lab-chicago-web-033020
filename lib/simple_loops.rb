0eeab3113a6b920631700c776cd33ca66f391003
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 06:17:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,11 @@
+ef loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
 def loop_message_n_times(message, limit)
   count = 0
   while count < limit do
