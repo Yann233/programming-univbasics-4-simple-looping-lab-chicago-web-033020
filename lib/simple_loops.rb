d88b5d3bfb10f0b39b165d0e04817288d7c05119
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 06:17:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
-def method_name(parameter)
+def loop_message_n_times(message, limit)
   count = 0
-  while count < (something)
-    # do something with the parameter
+  while count < limit do
+    puts message
     count += 1
   end
 end
